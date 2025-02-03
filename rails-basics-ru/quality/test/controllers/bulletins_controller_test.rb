e8479e9d25e54ad84f6e5501d9ded2e980046ad4
@hexlet/cli <support@hexlet.io>
2025-02-03 11:07:02 +0300

    submit rails-basics-ru/quality

--- rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -2,9 +2,22 @@ require 'test_helper'
 
 class BulletinsControllerTest < ActionDispatch::IntegrationTest
 
-  test '#index' do
+  test '#root' do
     get root_path
     assert_response :success
   end
+
+  test '#index' do
+    get bulletins_url
+    assert_response :success
+    assert_select "h1", "Bulletins"
+  end
+  
+  test '#show' do
+    bulletin = bulletins(:bulletin_1)
+    get bulletin_path(bulletin)
+    assert_response :success
+    assert_select 'h1', text: bulletin.title
+  end 
 
 end
