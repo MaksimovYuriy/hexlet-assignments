a713144d1991e5c75ad9c1d68ff7f2b038b5dd29
@hexlet/cli <support@hexlet.io>
2025-02-05 16:41:36 +0300

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/app/javascript/application.js
+++ rails-basics-ru/crud/app/javascript/application.js
@@ -1,7 +1,2 @@
 import "@hotwired/turbo-rails"
-import "../controllers"
 import * as bootstrap from "bootstrap"
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
